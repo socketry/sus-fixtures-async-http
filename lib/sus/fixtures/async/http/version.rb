e26f1b8a39c8ade428cfb89f27e4f75cef25296a
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2023-02-09 16:24:06 +1300

    Don't depend on other gems in `version.rb`.

--- lib/sus/fixtures/async/http/version.rb
+++ lib/sus/fixtures/async/http/version.rb
@@ -3,12 +3,12 @@
 # Released under the MIT License.
 # Copyright, 2022, by Samuel Williams.
 
-require 'sus/fixtures'
-
-module Sus::Fixtures
-	module Async
-		module HTTP
-			VERSION = "0.2.3"
+module Sus
+	module Fixtures
+		module Async
+			module HTTP
+				VERSION = "0.2.3"
+			end
 		end
 	end
 end
